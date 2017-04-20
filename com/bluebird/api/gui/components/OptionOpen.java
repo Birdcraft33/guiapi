721f43c7d27dc372f00e26d24ddcccc992645e07
Birdcraft33 <BlueBird-LM@web.de>
2017-04-20 13:09:46 +0200

    Create OptionOpen.java

---
+++ com/bluebird/api/gui/components/OptionOpen.java
@@ -0,0 +1,24 @@
+package com.bluebird.api.gui.components;
+
+/**
+ * <h2>general</h2>
+ * The OptionOpen set the type an {@link Option}Component (also the {@link YesNoOption}) will open
+ * 
+ * <h2>types</h2>
+ * <dt>underInventory</dt>
+ * UnderInventory means that the Inventory will be one line bigger and in this new Line are all options<br>
+ * <dt>newInventory</dt>
+ * NewInventory means that a new Inventory will be open for the options
+ * 
+ * @author Birdcraft33
+ * @category OpenType
+ * @version 1.0
+ * @since 1.0
+ * @extends no
+ * @initialize no
+ */
+public enum OptionOpen {
+
+	underInventory, newInventory;
+	
+}
